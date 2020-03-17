2dff9a6d2187f775baf07506c07d2dfbb7def3b3
mochisuna <hirokazu.maruta@gmail.com>
2020-03-17 11:35:52 +0900

    fix move configuration of ngrok hosts to development file.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,5 @@ module RubyGettingStarted
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.hosts << /[a-z0-9]+\.ngrok\.io/
   end
 end
